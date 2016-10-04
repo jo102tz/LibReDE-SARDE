9909143a515adb98748319a2a8f22eb6b9543213
Johannes Grohmann <Johannes Grohmann@DESKTOP-V3DJR2R>
2016-10-04 14:26:43 +0200

    added evaluator

--- tools.descartes.librede.rrde/src/tools/descartes/librede/rrde/eval/Evaluate.java
+++ tools.descartes.librede.rrde/src/tools/descartes/librede/rrde/eval/Evaluate.java
@@ -24,71 +24,81 @@
  * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  */
-package tools.descartes.librede.rrde.test;
+package tools.descartes.librede.rrde.eval;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Test;
 
 import tools.descartes.librede.Librede;
+import tools.descartes.librede.configuration.EstimationSpecification;
 import tools.descartes.librede.configuration.LibredeConfiguration;
+import tools.descartes.librede.rrde.OptimizedLibredeExecutor;
 import tools.descartes.librede.rrde.Plugin;
-import tools.descartes.librede.rrde.eval.TestSetValidator;
 import tools.descartes.librede.rrde.optimization.Discovery;
 import tools.descartes.librede.rrde.optimization.InputData;
 import tools.descartes.librede.rrde.optimization.OptimizationConfiguration;
 import tools.descartes.librede.rrde.optimization.RunCall;
 import tools.descartes.librede.rrde.optimization.Util;
 import tools.descartes.librede.rrde.recommendation.RecommendationTrainingConfiguration;
+import tools.descartes.librede.rrde.recommendation.algorithm.IRecomendationAlgorithm;
+import tools.descartes.librede.rrde.recommendation.extract.IFeatureExtractor;
+import tools.descartes.librede.rrde.test.AbstractTest;
 
 /**
  * @author JS
  *
  */
-public class AbstractTest {
+public class Evaluate {
+
+	/**
+	 * The logger used for logging
+	 */
+	private static final Logger log = Logger.getLogger(Evaluate.class);
+	
+	/**
+	 * A link to the desktop.
+	 */
+	public static final String DESKTOP = "C://Users//Johannes Grohmann//Desktop";
 
 	/**
 	 * The path linking to the test folder.
 	 */
-	public static final String TESTPATH = "resources" + File.separator + "test"
-			+ File.separator + "junit";
+	public static final String TESTPATH = "resources" + File.separator + "test" + File.separator + "validation";
 
 	/**
 	 * The path to the default {@link LibredeConfiguration}
 	 */
-	public final static String LIB_PATH = TESTPATH + File.separator
-			+ "estimation.librede";
+	public final static String LIB_PATH = TESTPATH + File.separator + "estimation.librede";
 
 	/**
 	 * The path to the default {@link OptimizationConfiguration}
 	 */
-	public final static String OPT_PATH = TESTPATH + File.separator
-			+ "conf.optimization";
+	public final static String OPT_PATH = TESTPATH + File.separator + "conf.optimization";
 
 	/**
 	 * The path to the default {@link RecommendationTrainingConfiguration}
 	 */
-	public final static String REC_PATH = TESTPATH + File.separator
-			+ "conf.recommendation";
+	public final static String REC_PATH = TESTPATH + File.separator + "conf.recommendation";
 	/**
 	 * The path for validation
 	 */
-	public static final String validationfolder = TESTPATH + File.separator
-			+ "validation";
+	public static final String validationfolder = DESKTOP + File.separator + "validation";
 
 	/**
 	 * The path for training
 	 */
-	public static final String trainingfolder = TESTPATH + File.separator
-			+ "training";
+	public static final String trainingfolder = DESKTOP + File.separator + "training";
 
 	/**
 	 * The output path, where all output files are stored.
 	 */
-	public final static String OUTPUT = TESTPATH + File.separator + "output";
+	public final static String OUTPUT = DESKTOP + File.separator + "output";
 
 	/**
 	 * The testset used for validation.
@@ -120,28 +130,24 @@ public class AbstractTest {
 	 */
 	static Plugin main;
 
-	@BeforeClass
-	public static void init() {
+	@Test
+	public void test() {
+		log.info("Starting initialization");
+
 		main = new Plugin();
 		main.init();
 		// load config files
 		librede = Librede.loadConfiguration(new File(LIB_PATH).toPath());
-		optimization = Util.loadOptimizationConfiguration(new File(OPT_PATH)
-				.toPath());
-		recommendation = Util
-				.loadRecommendationConfiguration(new File(REC_PATH).toPath());
+		optimization = Util.loadOptimizationConfiguration(new File(OPT_PATH).toPath());
+		recommendation = Util.loadRecommendationConfiguration(new File(REC_PATH).toPath());
 
 		// discover validation configurations
 		for (InputData data : recommendation.getTrainingData()) {
 			data.setRootFolder(validationfolder);
 		}
 
-		configs = Discovery.createConfigurations(
-				recommendation.getTrainingData(), librede.getEstimation(),
+		configs = Discovery.createConfigurations(recommendation.getTrainingData(), librede.getEstimation(),
 				librede.getValidation());
-		// check if size is correct
-		Assert.assertEquals(new File(validationfolder).list().length,
-				configs.size());
 
 		// adapt configurations to be similar
 		for (InputData data : recommendation.getTrainingData()) {
@@ -160,6 +166,33 @@ public class AbstractTest {
 		vali = new TestSetValidator(configs);
 		Assert.assertNotEquals(vali.getTestset().size(), 0);
 		vali.calculateInitialErrors();
+		log.info("Initialized! Starting optimization...");
+		long start = System.currentTimeMillis();
+		// run optimization
+		Collection<EstimationSpecification> estimations = new tools.descartes.librede.rrde.optimization.Plugin()
+				.runConfigurationOptimization(librede, optimization, OUTPUT);
+		long opti = System.currentTimeMillis() - start;
+		log.info("Finished optimization! Starting training phase...");
+
+		// delete the read estimators and replace them with the optimized
+		// ones
+		recommendation.getEstimators().clear();
+		recommendation.getEstimators().addAll(estimations);
+
+		// train algorithm
+		start = System.currentTimeMillis();
+		IRecomendationAlgorithm algorithm = new tools.descartes.librede.rrde.recommendation.Plugin()
+				.loadAndTrainAlgorithm(recommendation);
+		IFeatureExtractor extractor = tools.descartes.librede.rrde.recommendation.Plugin
+				.loadFeatureExtractor(recommendation.getFeatureAlgorithm());
+		long reco = System.currentTimeMillis() - start;
+		log.info("Finished training! Validating...");
+
+		// wrap into Executor
+		OptimizedLibredeExecutor exec = new OptimizedLibredeExecutor(extractor, algorithm);
+		// print results
+		vali.compareOptimized(exec);
+		vali.printResults(null, opti, reco);
 
 	}
 
