dda0d4ecf477eb8e6bfed93cb365a93716f7b58f
Johannes Grohmann <johannes.grohmann@uni-wuerzburg.de>
2019-07-04 17:36:36 +0200

    added Java version of S3

---
+++ tools.descartes.librede.rrde.optimization/src/tools/descartes/librede/rrde/optimization/algorithm/impl/IterativeParameterOptimizationAlgorithmJava.java
@@ -0,0 +1,199 @@
+/**
+ * ==============================================
+ *  LibReDE : Library for Resource Demand Estimation
+ * ==============================================
+ *
+ * (c) Copyright 2013-2014, by Simon Spinner and Contributors.
+ *
+ * Project Info:   http://www.descartes-research.net/
+ *
+ * All rights reserved. This software is made available under the terms of the
+ * Eclipse Public License (EPL) v1.0 as published by the Eclipse Foundation
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse Public License (EPL)
+ * for more details.
+ *
+ * You should have received a copy of the Eclipse Public License (EPL)
+ * along with this software; if not visit http://www.eclipse.org or write to
+ * Eclipse Foundation, Inc., 308 SW First Avenue, Suite 110, Portland, 97204 USA
+ * Email: license (at) eclipse.org
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ */
+package tools.descartes.librede.rrde.optimization.algorithm.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
+
+import tools.descartes.librede.rrde.model.optimization.ConfigurationOptimizationAlgorithmSpecifier;
+import tools.descartes.librede.rrde.model.optimization.IOptimizableParameter;
+import tools.descartes.librede.rrde.model.optimization.impl.IterativeParameterOptimizerSpecifierImpl;
+import tools.descartes.librede.rrde.optimization.algorithm.AbstractConfigurationOptimizer;
+import tools.descartes.librede.rrde.rinterface.ICallbackEvaluator;
+import tools.descartes.librede.rrde.rinterface.RBridge;
+import tools.descartes.librede.rrde.util.Util;
+
+/**
+ * This algorithm is an all-Java implementation of the
+ * {@link IterativeParameterOptimizationAlgorithm} algorithm in order to avoid
+ * compatibility problems with the R bridge.
+ * 
+ * @author Johannes Grohmann (johannes.grohmann@uni-wuerzburg.de)
+ *
+ */
+public class IterativeParameterOptimizationAlgorithmJava extends AbstractConfigurationOptimizer {
+
+	/**
+	 * The log used for logging.
+	 */
+	private static final Logger log = Logger.getLogger(IterativeParameterOptimizationAlgorithmJava.class);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see tools.descartes.librede.rrde.optimization.IConfigurationOptimizer#
+	 * getSimpleName()
+	 */
+	@Override
+	public String getSimpleName() {
+		return this.getClass().getSimpleName();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see tools.descartes.librede.rrde.optimization.IConfigurationOptimizer#
+	 * isSpecifierSupported(tools.descartes.librede.rrde.optimization.
+	 * IConfigurationOptimizationAlgorithmSpecifier)
+	 */
+	@Override
+	public boolean isSpecifierSupported(ConfigurationOptimizationAlgorithmSpecifier specifier) {
+		if (specifier instanceof IterativeParameterOptimizerSpecifierImpl) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see tools.descartes.librede.rrde.optimization.AbstractConfigurationOptimizer
+	 * #getLog()
+	 */
+	@Override
+	protected Logger getLog() {
+		return log;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see tools.descartes.librede.rrde.optimization.AbstractConfigurationOptimizer
+	 * #executeAlgorithm()
+	 */
+	@Override
+	public void executeAlgorithm() {
+		StepwiseSamplingSearch search = new StepwiseSamplingSearch();
+		IterativeParameterOptimizerSpecifierImpl alg = (IterativeParameterOptimizerSpecifierImpl) getAlgorithm();
+		getLog().info("Starting execution of stepwise sampling search (S3)...");
+
+		// calculate expected runtime
+		long complexity = (long) (alg.getNumberOfIterations() * alg.getNumberOfExplorations()
+				* Math.pow(alg.getNumberOfSplits() + 2, getSettings().getParametersToOptimize().size()));
+		String timestamp = DurationFormatUtils.formatDurationHMS(complexity * getFirstiterationtime());
+		getLog().info("Expected runtime (pessimistic): " + timestamp);
+
+		// call RBridge
+		ICallbackEvaluator evaluator = new ICallbackEvaluator() {
+			@Override
+			public double evaluate(Map<IOptimizableParameter, Double> params) {
+				for (Entry<IOptimizableParameter, Double> en : params.entrySet()) {
+					setTargetValue(en.getKey(), en.getValue());
+				}
+				return runIteration();
+			}
+		};
+		Map<IOptimizableParameter, Double> best = search.runOptimization(getSettings().getParametersToOptimize(),
+				evaluator, alg.getNumberOfSplits(), alg.getNumberOfExplorations(), alg.getNumberOfIterations());
+		// do sanity check first
+		doSanityCheck(best, evaluator);
+		// set target values
+		for (Entry<IOptimizableParameter, Double> en : best.entrySet()) {
+			log.info("Found parameter value of " + en.getValue() + " for parameter "
+					+ Util.getParameterString(en.getKey()));
+			setTargetValue(en.getKey(), en.getValue());
+		}
+		getLog().info("Finished execution of IPA script!");
+	}
+
+	/**
+	 * Performs the sanity check, i.e. checks, if the results would be better if any
+	 * of the parameters would go back to default values.
+	 * 
+	 * @param best      The currently best parameter configuration.
+	 * @param evaluator The evaluator to evaluate the parameter changes.
+	 */
+	private void doSanityCheck(Map<IOptimizableParameter, Double> best, ICallbackEvaluator evaluator) {
+		getLog().trace("Performing sanity check.");
+		for (Entry<IOptimizableParameter, Double> en : best.entrySet()) {
+			getLog().trace("Parameter " + Util.getParameterString(en.getKey()) + " has value " + en.getValue() + ".");
+		}
+		int iteration = 0;
+		// repeat until no improvement can be found
+		boolean changed = false;
+		do {
+			getLog().trace("Iteration no. " + iteration++);
+			Map<IOptimizableParameter, Double> map = getBetterMap(best, evaluator, evaluator.evaluate(best));
+			if (map == null) {
+				// if map is null, no improvements could be found
+				break;
+			} else {
+				best = map;
+				getLog().trace("Found improvement by using the default value.");
+				changed = true;
+			}
+		} while (changed);
+
+		getLog().trace("No (more) improvements to be found. Existing sanity check.");
+	}
+
+	/**
+	 * Checks, if the results would be better if any of the parameters would go back
+	 * to default values.
+	 * 
+	 * @param best      The currently best parameter configuration.
+	 * @param evaluator The evaluator to evaluate the parameter changes.
+	 * @param target    The error value to beat.
+	 * @return A map that has a better error value than <b>target</b>, or
+	 *         <code>null</code> if none could be found.
+	 */
+	private Map<IOptimizableParameter, Double> getBetterMap(Map<IOptimizableParameter, Double> best,
+			ICallbackEvaluator evaluator, double target) {
+		Map<IOptimizableParameter, Double> returnmap = null;
+		for (Entry<IOptimizableParameter, Double> en : best.entrySet()) {
+			getLog().trace("Parameter " + Util.getParameterString(en.getKey()) + " has value " + en.getValue() + ".");
+			Double valTmp = en.getValue();
+			// insert given default value
+			best.put(en.getKey(), en.getKey().getStartValue());
+			double compare = evaluator.evaluate(best);
+			if (compare < target) {
+				returnmap = new HashMap<IOptimizableParameter, Double>(best);
+				// update target
+				target = compare;
+			}
+			// restore default
+			best.put(en.getKey(), valTmp);
+		}
+		return returnmap;
+	}
+
+}
---
+++ tools.descartes.librede.rrde.optimization/src/tools/descartes/librede/rrde/optimization/algorithm/impl/StepwiseSamplingSearch.java
@@ -0,0 +1,259 @@
+/**
+ * ==============================================
+ *  LibReDE : Library for Resource Demand Estimation
+ * ==============================================
+ *
+ * (c) Copyright 2013-2014, by Simon Spinner and Contributors.
+ *
+ * Project Info:   http://www.descartes-research.net/
+ *
+ * All rights reserved. This software is made available under the terms of the
+ * Eclipse Public License (EPL) v1.0 as published by the Eclipse Foundation
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse Public License (EPL)
+ * for more details.
+ *
+ * You should have received a copy of the Eclipse Public License (EPL)
+ * along with this software; if not visit http://www.eclipse.org or write to
+ * Eclipse Foundation, Inc., 308 SW First Avenue, Suite 110, Portland, 97204 USA
+ * Email: license (at) eclipse.org
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
+ * in the United States and other countries.]
+ */
+package tools.descartes.librede.rrde.optimization.algorithm.impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.eclipse.emf.common.util.EList;
+
+import tools.descartes.librede.rrde.model.optimization.IOptimizableParameter;
+import tools.descartes.librede.rrde.rinterface.ICallbackEvaluator;
+
+/**
+ * This algorithm is an all-Java implementation of the
+ * {@link IterativeParameterOptimizationAlgorithm} algorithm in order to avoid
+ * compatibility problems with the R bridge.
+ * 
+ * The algorithm is described in: Qais Noorshams, Dominik Bruhn, Samuel Kounev,
+ * and Ralf Reussner. Predictive Performance Modeling of Virtualized Storage
+ * Systems using Optimized Statistical Regression Techniques. In Proceedings of
+ * the ACM/SPEC International Conference on Performance Engineering (ICPE 2013),
+ * Prague, Czech Republic, 2013, ICPE'13, pages 283--294. ACM, New York, NY,
+ * USA. 2013.
+ * 
+ * @author Johannes Grohmann (johannes.grohmann@uni-wuerzburg.de)
+ *
+ */
+public class StepwiseSamplingSearch {
+
+	/**
+	 * Executes the actual S3 algorithm.
+	 * 
+	 * @param parametersToOptimize The list of parameters to optimize.
+	 * @param evaluator            The evaluator in order to perform parameter
+	 *                             changes on the data.
+	 * @param numberOfSplits       The number of splits (S3 parameter).
+	 * @param numberOfExplorations The number of exploration points (S3 parameter).
+	 * @param numberOfIterations   The number of iterations to perform (S3
+	 *                             parameter).
+	 * @return A Map containing the best found parameter configuration.
+	 */
+	public Map<IOptimizableParameter, Double> runOptimization(EList<IOptimizableParameter> parametersToOptimize,
+			ICallbackEvaluator evaluator, int numberOfSplits, int numberOfExplorations, int numberOfIterations) {
+
+		// initializing
+		SortedSet<EvaluationPoint> results = new TreeSet<EvaluationPoint>();
+		SortedSet<EvaluationPoint> bestSet = new TreeSet<EvaluationPoint>();
+		// evaluate first border parameters
+		Map<IOptimizableParameter, Double> parameterset = new HashMap<IOptimizableParameter, Double>();
+		for (IOptimizableParameter param : parametersToOptimize) {
+			parameterset.put(param, param.getLowerBound());
+		}
+		ParameterCombination start = new ParameterCombination(parameterset);
+		Double eval = evaluator.evaluate(start.getParams());
+		results.add(new EvaluationPoint(start, eval));
+		bestSet.add(new EvaluationPoint(start, eval));
+
+		// while stopping criterion is not fullfilled
+		for (int iteration = 0; iteration < numberOfIterations; iteration++) {
+			for (EvaluationPoint evaluationPoint : bestSet) {
+				Map<IOptimizableParameter, List<Double>> parameterPoints = new HashMap<IOptimizableParameter, List<Double>>();
+				for (IOptimizableParameter param : parametersToOptimize) {
+					double minval = findClosestMinVal(param, evaluationPoint, results);
+					double maxval = findClosestMaxVal(param, evaluationPoint, results);
+					double stepSize = (maxval - minval) / (numberOfSplits + 1);
+					List<Double> parampoints = new ArrayList<Double>(numberOfSplits + 1);
+					for (int j = 0; j < numberOfSplits + 2; j++) {
+						parampoints.add(minval + j * stepSize);
+					}
+					parameterPoints.put(param, parampoints);
+				}
+				Set<ParameterCombination> crossProduct = buildCartesianProduct(parameterPoints);
+				addEvaluationPoints(evaluator, results, crossProduct);
+				// TODO Rounding
+			}
+			// find new best set
+			bestSet.clear();
+			Iterator<EvaluationPoint> bestIterator = results.iterator();
+			for (int i = 0; i < numberOfExplorations; i++) {
+				bestSet.add(bestIterator.next());
+			}
+		}
+		return results.first().getParam().getParams();
+	}
+
+	private void addEvaluationPoints(ICallbackEvaluator evaluator, SortedSet<EvaluationPoint> results,
+			Set<ParameterCombination> crossProduct) {
+		for (ParameterCombination parameterCombination : crossProduct) {
+			if (!alreadyEvaluated(results, parameterCombination)) {
+				double error = evaluator.evaluate(parameterCombination.getParams());
+				results.add(new EvaluationPoint(parameterCombination, error));
+			}
+		}
+	}
+
+	private boolean alreadyEvaluated(SortedSet<EvaluationPoint> results, ParameterCombination parameterCombination) {
+		for (EvaluationPoint evaluationPoint : results) {
+			if (evaluationPoint.getParam().equals(parameterCombination))
+				return true;
+		}
+		return false;
+	}
+
+	private Set<ParameterCombination> buildCartesianProduct(Map<IOptimizableParameter, List<Double>> parameterPoints) {
+		Set<Map<IOptimizableParameter, Double>> product = cartesianProduct(copy(parameterPoints));
+		Set<ParameterCombination> combination = new HashSet<ParameterCombination>();
+		for (Map<IOptimizableParameter, Double> set : product) {
+			combination.add(new ParameterCombination(set));
+		}
+		return combination;
+	}
+
+	private Set<Map<IOptimizableParameter, Double>> cartesianProduct(Map<IOptimizableParameter, List<Double>> listmap) {
+		Set<Map<IOptimizableParameter, Double>> ret = new HashSet<Map<IOptimizableParameter, Double>>();
+		if (listmap.isEmpty()) {
+			ret.add(new HashMap<IOptimizableParameter, Double>());
+		} else {
+			// get next param;
+			IOptimizableParameter para = listmap.keySet().iterator().next();
+			List<Double> values = listmap.remove(para);
+			for (Double val : values) {
+				for (Map<IOptimizableParameter, Double> map : cartesianProduct(copy(listmap))) {
+					map.put(para, val);
+					ret.add(map);
+				}
+			}
+		}
+		return ret;
+	}
+
+	private Map<IOptimizableParameter, List<Double>> copy(Map<IOptimizableParameter, List<Double>> listmap) {
+		Map<IOptimizableParameter, List<Double>> c = new HashMap<IOptimizableParameter, List<Double>>();
+		c.putAll(listmap);
+		return c;
+	}
+
+	private double findClosestMaxVal(IOptimizableParameter param, EvaluationPoint evaluationPoint,
+			SortedSet<EvaluationPoint> results) {
+		// get evaluation point val
+		double currVal = evaluationPoint.getParam().getParams().get(param);
+		// search for closest value that is bigger of the evaluation set
+		// if no bigger value can be found -> choose maximum value
+		double smallestMax = param.getUpperBound();
+		for (EvaluationPoint point : results) {
+			double val = point.getParam().getParams().get(param);
+			// if value is actually bigger than currVal, but also smaller than the current
+			// smallest max -> new smallest max
+			if ((val > currVal) && (val < smallestMax)) {
+				smallestMax = val;
+			}
+		}
+		return smallestMax;
+	}
+
+	private double findClosestMinVal(IOptimizableParameter param, EvaluationPoint evaluationPoint,
+			SortedSet<EvaluationPoint> results) {
+		// get evaluation point val
+		double currVal = evaluationPoint.getParam().getParams().get(param);
+		// search for closest value that is smaller of the evaluation set
+		// if no smaller value can be found -> choose minimum value
+		double highestMin = param.getLowerBound();
+		for (EvaluationPoint point : results) {
+			double val = point.getParam().getParams().get(param);
+			// if value is actually smaller than currVal, but also higher than the current
+			// highest min -> new highest min
+			if ((val < currVal) && (val > highestMin)) {
+				highestMin = val;
+			}
+		}
+		return highestMin;
+	}
+
+	private class ParameterCombination {
+		Map<IOptimizableParameter, Double> params;
+
+		/**
+		 * @return the params
+		 */
+		public Map<IOptimizableParameter, Double> getParams() {
+			return params;
+		}
+
+		/**
+		 * @param params
+		 */
+		public ParameterCombination(Map<IOptimizableParameter, Double> params) {
+			super();
+			this.params = params;
+		}
+	}
+
+	private class EvaluationPoint implements Comparable<EvaluationPoint> {
+		ParameterCombination param;
+		Double value;
+
+		/**
+		 * @param param
+		 * @param value
+		 */
+		public EvaluationPoint(ParameterCombination param, Double value) {
+			super();
+			this.param = param;
+			this.value = value;
+		}
+
+		/**
+		 * @return the param
+		 */
+		public ParameterCombination getParam() {
+			return param;
+		}
+
+		/**
+		 * @return the value
+		 */
+		public Double getValue() {
+			return value;
+		}
+
+		@Override
+		public int compareTo(EvaluationPoint o) {
+			// a value is better, if it is smaller, therefore reverse the comparison
+			return o.getValue().compareTo(this.getValue());
+		}
+	}
+
+}
